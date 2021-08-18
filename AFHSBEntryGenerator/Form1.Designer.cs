cb55712319908577a67b516e705664ec30852873
Christian Armstrong <armstrongc@uspioneer.com>
2021-08-18 15:07:58 -0700

    [master] Added an engine to compare PHA and AFHSB field lengths

--- AFHSBEntryGenerator/Form1.Designer.cs
+++ AFHSBEntryGenerator/Form1.Designer.cs
@@ -31,43 +31,59 @@
             this.txt_Mappings = new System.Windows.Forms.TextBox();
             this.lbl_Path = new System.Windows.Forms.Label();
             this.btn_Generate = new System.Windows.Forms.Button();
+            this.btn_CompareValidValues = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txt_Mappings
             // 
-            this.txt_Mappings.Location = new System.Drawing.Point(56, 70);
+            this.txt_Mappings.Location = new System.Drawing.Point(42, 57);
+            this.txt_Mappings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Mappings.Name = "txt_Mappings";
-            this.txt_Mappings.Size = new System.Drawing.Size(465, 22);
+            this.txt_Mappings.Size = new System.Drawing.Size(350, 20);
             this.txt_Mappings.TabIndex = 0;
             // 
             // lbl_Path
             // 
             this.lbl_Path.AutoSize = true;
-            this.lbl_Path.Location = new System.Drawing.Point(56, 49);
+            this.lbl_Path.Location = new System.Drawing.Point(42, 40);
+            this.lbl_Path.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_Path.Name = "lbl_Path";
-            this.lbl_Path.Size = new System.Drawing.Size(286, 17);
+            this.lbl_Path.Size = new System.Drawing.Size(215, 13);
             this.lbl_Path.TabIndex = 1;
             this.lbl_Path.Text = "Path to folder with Mapping and Translation:";
             // 
             // btn_Generate
             // 
-            this.btn_Generate.Location = new System.Drawing.Point(586, 66);
+            this.btn_Generate.Location = new System.Drawing.Point(440, 54);
+            this.btn_Generate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Generate.Name = "btn_Generate";
-            this.btn_Generate.Size = new System.Drawing.Size(93, 31);
+            this.btn_Generate.Size = new System.Drawing.Size(75, 25);
             this.btn_Generate.TabIndex = 3;
             this.btn_Generate.Text = "Generate";
             this.btn_Generate.UseVisualStyleBackColor = true;
             this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
             // 
+            // btn_CompareValidValues
+            // 
+            this.btn_CompareValidValues.Location = new System.Drawing.Point(440, 85);
+            this.btn_CompareValidValues.Name = "btn_CompareValidValues";
+            this.btn_CompareValidValues.Size = new System.Drawing.Size(75, 37);
+            this.btn_CompareValidValues.TabIndex = 4;
+            this.btn_CompareValidValues.Text = "Compare Valid Values";
+            this.btn_CompareValidValues.UseVisualStyleBackColor = true;
+            this.btn_CompareValidValues.Click += new System.EventHandler(this.btn_CompareValidValues_Click);
+            // 
             // Form1
             // 
             this.AcceptButton = this.btn_Generate;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 150);
+            this.ClientSize = new System.Drawing.Size(555, 207);
+            this.Controls.Add(this.btn_CompareValidValues);
             this.Controls.Add(this.btn_Generate);
             this.Controls.Add(this.lbl_Path);
             this.Controls.Add(this.txt_Mappings);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,6 +96,7 @@
         private System.Windows.Forms.TextBox txt_Mappings;
         private System.Windows.Forms.Label lbl_Path;
         private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.Button btn_CompareValidValues;
     }
 }
 
---
+++ AFHSBEntryGenerator/InterAgencyFieldMapping.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFHSBEntryGenerator
+{
+    public class InterAgencyFieldMapping
+    {
+        //Properties        
+        /// <summary>
+        /// The PHA fieldname
+        /// </summary>
+        public string EpiDataCenter;
+
+        public int EpiDataCenterFieldLength { get; set; }
+
+        /// <summary>
+        /// The ahlta fieldname
+        /// </summary>
+        public string AFHSC;
+
+        public int AfhscFieldLength { get; set; }
+
+        /// <summary>
+        /// The Acs-Dal fieldname
+        /// </summary>
+        public string ACSDAL;
+
+        public int AcsDalFieldLength { get; set; }
+
+        /// <summary>
+        /// SegmenArea Associate with the field
+        /// </summary>
+        public AssessmentSegmentArea SegmentArea;
+    }
+
+    [Flags]
+    public enum AssessmentSegmentArea
+    {
+        ServiceMember = 1,
+        RecordReviewer = 2,
+        MHAProvider = 4,
+        HCP = 8,
+        Other = 16,
+        IndexTableFields = 32
+    }
+}
